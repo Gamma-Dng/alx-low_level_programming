606cfd45a495578ef88e11c5297ca852e1b63e05
Gamma-Dng <briandundu@gmail.com>
2023-06-15 16:26:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char prog[] = ("\"Programming is like building a multilingual puzzle");
+puts(prog);
+return (0);
+}
