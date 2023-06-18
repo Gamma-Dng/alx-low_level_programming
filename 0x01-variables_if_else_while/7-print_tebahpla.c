a3423c6d766bf95edc5efb36cf43862bc5b75db6
Gamma-Dng <briandundu@gmail.com>
2023-06-17 18:31:15 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: (0) (success)
+ */
+int main(void)
+{int b = 'z';
+for (b ; b >= 'a'; b--)
+{
+putchar(b);
+putchar('\n');
+}
+return (0);
+}
+
+
