6c530f18c999393b22f41abe7f630c79a4c10c31
Gamma-Dng <briandundu@gmail.com>
2023-06-20 17:10:29 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: (0) (success)
+ */
+void print_alphabet(void);
+{
+int a;
+for (a = "a"; a <= "z"; a++)
+_putchar(a);
+}
+_putchar('\n');
+}
