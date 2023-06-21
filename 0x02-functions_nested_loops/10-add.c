a8d01895913d68ca63b7b878c415f73ba5f2c1cc
Gamma-Dng <briandundu@gmail.com>
2023-06-21 06:44:36 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two values
+ * @a: first number entered
+ * @b: second number entered
+ * Return: (integer value)
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
