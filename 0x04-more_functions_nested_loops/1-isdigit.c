3d299012c941e901d7adc03ba4dd0cfaa33c244f
Gamma-Dng <briandundu@gmail.com>
2023-06-22 12:23:30 -0700

    ./1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_isdigit - Checks for a digit
+ *@c: Character computed for check
+ *Return: (1) for digit and (0) for any other
+ */
+
+int _isdigit(int c)
+{
+while (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
+
+
+
+
+
+
+
