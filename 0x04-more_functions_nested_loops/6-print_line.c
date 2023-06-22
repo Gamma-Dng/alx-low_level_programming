dfdf05350cd94aeff79c68e58755b238f7bae344
Gamma-Dng <briandundu@gmail.com>
2023-06-22 14:53:49 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_line - Prints lines
+ *@n:number of checks
+ *Return: (integer)
+ */
+void print_line(int n)
+{
+int k = 0;
+while (k < n)
+{
+_putchar('_');
+k++;
+}
+_putchar('\n');
+}
