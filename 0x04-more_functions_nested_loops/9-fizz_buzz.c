ec55f3a2a6029226ea545ff42c1a9f42eb734cfc
Gamma-Dng <briandundu@gmail.com>
2023-06-22 17:10:52 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 int k;
-{
 for (k = 1; k <= 100; k++)
+{
 if (k % 3 == 0)
 printf("Fizz");
 else if (k % 5 == 0)
@@ -16,15 +16,11 @@ printf("Buzz");
 else if (k % 15 == 0)
 printf("FizzBuzz");
 else
-{
 printf("%d", k);
-}
 if (k < 100)
-{
 printf(" ");
 }
 printf("\n");
-}
 return (0);
 }
 
