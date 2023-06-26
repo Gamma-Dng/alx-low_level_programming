8b480718ba5fbbc57cac2c8801973ad43b76a4ca
Gamma-Dng <briandundu@gmail.com>
2023-06-26 12:06:19 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+  *rev_string - prints a string in reverse
+  *@s: string characters
+  *Return: (string)
+  */
+void rev_string(char *s)
+{
+	int length = 0, index = 0;
+char temp;
+while(s[index++])
+length++;
+for (index = length - 1; index >= length / 2; index--)
+{
+temp = s[index];
+s[index] = s[length - index - 1];
+s[length - index - 1] = temp;
+}
+}
