86d75f99fcf0ff6f63fc903dd2e0fee5282d9d7e
Gamma-Dng <briandundu@gmail.com>
2023-07-13 11:04:57 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,27 +10,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *str;
-unsigned int i, j, s1len, s2len;
+char *concat;
+unsigned int len = n, index;
+
 if (s1 == NULL)
 s1 = "";
+
 if (s2 == NULL)
 s2 = "";
-for (s1len = 0; s1[s1len] != '\0'; s1len++)
-for (s2len = 0; s2[s2len] != '\0'; s2len++)
-str = malloc(s1len + n + 1);
-if (str == NULL)
-{
-return (NULL);
-}
-for (i = 0; s1[i] != '\0'; i++)
-str[i] = s1[i];
-for (j = 0; j < n; j++)
-{
-str[i] = s2[j];
-i++;
-}
-str[i] = '\0';
-return (str);
-}
 
+for (index = 0; s1[index]; index++)
+len++;
+
+concat = malloc(sizeof(char) * (len + 1));
+
+if (concat == NULL)
+return (NULL);
+
+len = 0;
+
+for (index = 0; s1[index]; index++)
+concat[len++] = s1[index];
+
+for (index = 0; s2[index] && index < n; index++)
+concat[len++] = s2[index];
+
+concat[len] = '\0';
+
+return (concat);
+}
