5a1c4873009e183db1cf6f22c0d2ad42764fd658
Gamma-Dng <briandundu@gmail.com>
2023-09-05 04:48:28 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,25 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: arguments count
+ * @av: arguments vector
+ *
+ * Return: a pointer to a new string, or NULL if it fails
  */
-char *_strdup(char *str)
+char *argstostr(int ac, char **av)
 {
-char *b;
-int i, r = 0;
-if (str == NULL)
-return (NULL);
-i = 0;
-while (str[i] != '\0')
-i++;
-b = malloc(sizeof(char) * (i + 1));
-if (b == NULL)
-return (NULL);
-for (r = 0; str[r]; r++)
-b[r] = str[r];
-return (b);
+	char *str, *s;
+	int i, j, k, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		s = av[i];
+		j = 0;
+
+		while (s[j++])
+			len++;
+		len++;
+	}
+
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0, j = 0; i < ac && j < len; i++)
+	{
+		s = av[i];
+		k = 0;
+
+		while (s[k])
+		{
+			str[j] = s[k];
+			k++;
+			j++;
+		}
+		str[j++] = '\n';
+	}
+	str[j] = '\0';
+
+	return (str);
 }
 
