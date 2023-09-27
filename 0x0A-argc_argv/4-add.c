f1cd1fe73f729aadbf2d9fd93f8257a81786ac10
Gamma-Dng <briandundu@gmail.com>
2023-09-27 12:00:13 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,52 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+
 /**
-* check_num - check - string there are digit
-* @str: array str
-* Return: Always 0 (Success)
-*/
-int check_num(char *str)
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
 {
-unsigned int count;
-count = 0;
-while (count < strlen(str))
-{
-if (!isdigit(str[count]))
-{
-return (0);
-}
-count++;
-}
-return (1);
-}
-/**
-* main - Print the name of the program
-* @argc: Count arguments
-* @argv: Arguments
-* Return: Always 0 (Success)
-*/
-int main(int argc, char *argv[])
-{
-int count;
-int str_to_int;
-int sum = 0;
-count = 1;
-while (count < argc)
-{
-if (check_num(argv[count]))
-{
-str_to_int = atoi(argv[count]);
-sum += str_to_int;
-}
-{
-printf("Error\n");
-return (1);
-}
-count++;
-}
-printf("%d\n", sum); /*print sum*/
-return (0);
+	int i, n, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
 }
 
